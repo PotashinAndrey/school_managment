4b1f41d75e7e955153480b46ea2f55d31ce497d7
Potashin Andrei <a.potashin@pnlfin.tech>
2024-10-05 20:08:42 +0300

    did menu from static object

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Menu from "@/components/Menu";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -8,13 +10,17 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      <div className="w-[16%] md:w-[8%] lg:w-[18%] xl:w-[16%] bg-red-100">
-        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2 p-4">
+      <div className="w-[16%] md:w-[8%] lg:w-[18%] xl:w-[16%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
           <Image src="/logo.png" alt="Logo" width="32" height="32" />
           <span className="hidden lg:block">Logo will me here</span>
         </Link>
+        <Menu />
       </div>
-      <div className="w-[84%] md:w-[92%] lg:w-[82%] xl:w-[84%] bg-blue-100">
+      <div className="w-[84%] md:w-[92%] lg:w-[82%] xl:w-[84%]">
         {children}
       </div>
     </div>
