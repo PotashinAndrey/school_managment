3faf958b55f28b301f4c1ee26356f803517b2996
Potashin Andrei <a.potashin@pnlfin.tech>
2024-10-21 21:34:03 +0300

    add some styles for calendar

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -8,8 +8,6 @@ import { FC, useState } from "react";
 
 const localizer = momentLocalizer(moment);
 
-console.log(calendarEvents);
-
 const BigCalendar: FC = () => {
   const [view, setView] = useState<View>(Views.WORK_WEEK);
 
@@ -21,14 +19,16 @@ const BigCalendar: FC = () => {
     <Calendar
       localizer={localizer}
       events={calendarEvents}
-      startAccessor="start"
-      endAccessor="end"
+      startAccessor="startDate"
+      endAccessor="endDate"
+      titleAccessor="title"
+      allDayAccessor="allDay"
       views={["work_week", "day"]}
       view={view}
       onView={handleOnChangeView}
       style={{ height: "98%" }}
-      min={new Date(2007, 1, 0, 8, 0, 0)}
-      max={new Date(2007, 1, 0, 17, 0, 0)}
+      min={new Date(2027, 1, 0, 8, 0, 0)}
+      max={new Date(2027, 1, 0, 17, 0, 0)}
     />
   );
 };
